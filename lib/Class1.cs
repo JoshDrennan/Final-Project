ab7f49668710a7a575fb11de53af9ef629582fd2
Your Name <joshdfootball73@gmail.com>
2022-03-21 20:03:24 -0600

    I added some stuff

---
+++ lib/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace lib;
+public interface IPerson
+{
+    public string Name{get;}
+    public int Score{get; set;}
+}
+public class Person : IPerson
+{
+    public Person(string name)
+    {
+        Person player = new Person(name);
+    }
+    public string Name => Name;
+
+    public int Score 
+    { 
+        get => Score; 
+        set
+        {
+            Score = value;
+        }
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace lib;
+
+public class Tests
+{
+    
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+
+        //Assert.AreEqual("Brad", );
+    }
+}
