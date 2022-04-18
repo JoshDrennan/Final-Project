2b4078eba608e6f32b0bf15f61df60279d41223b
Your Name <joshdfootball73@gmail.com>
2022-04-18 08:45:38 -0600

    Test 1 fail

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -19,39 +19,39 @@ public class Tests
         Assert.AreEqual("Brad", TestPlayer1.Name);
     }
 
-    [Test]
-    public void Test2()
-    {
-        Player TestPlayer1 = new Player("Brad");
-        TestPlayer.Add(TestPlayer1);
-        Assert.AreEqual(1, TestPlayer.Count);
-    }
+    // [Test]
+    // public void Test2()
+    // {
+    //     Player TestPlayer1 = new Player("Brad");
+    //     TestPlayer.Add(TestPlayer1);
+    //     Assert.AreEqual(1, TestPlayer.Count);
+    // }
 
-    [Test]
-    public void TestPlayerCountInList()
-    {
-        Player TestPlayer2 = new Player("Josh");
-        TestPlayer.Add(TestPlayer2);
-        Assert.AreEqual(2, TestPlayer.Count);
-    }
+    // [Test]
+    // public void TestPlayerCountInList()
+    // {
+    //     Player TestPlayer2 = new Player("Josh");
+    //     TestPlayer.Add(TestPlayer2);
+    //     Assert.AreEqual(2, TestPlayer.Count);
+    // }
 
-    [Test]
-    public void TestFramesArrayLength()
-    {
-        Frames testFrames = new Frames();
-        Assert.AreEqual(10, testFrames.frames.Length);
-    }
+    // [Test]
+    // public void TestFramesArrayLength()
+    // {
+    //     Frames testFrames = new Frames();
+    //     Assert.AreEqual(10, testFrames.frames.Length);
+    // }
 
-    [Test]
-    public void TestWhatIsInFramesArray()
-    {
-        Frames testFrames2 = new Frames();
-        Assert.AreEqual(0, testFrames2.frames[0]);
-    }
+    // [Test]
+    // public void TestWhatIsInFramesArray()
+    // {
+    //     Frames testFrames2 = new Frames();
+    //     Assert.AreEqual(0, testFrames2.frames[0]);
+    // }
 
-    [Test]
-    public void TestCalculateScore()
-    {
-        
-    }
+    // [Test]
+    // public void TestCalculateScore()
+    // {
+    //     Assert.AreEqual(6, GameLogic.CalculateScore(3, 3));
+    // }
 }
